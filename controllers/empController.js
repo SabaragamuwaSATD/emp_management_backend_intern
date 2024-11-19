a9f5b17bbac970ba86e992a437942e655259cc94
sabaragamuwaSATD <it21836268@my.sliit.lk>
2024-11-19 21:37:10 +0530

    emp delete function done

--- controllers/empController.js
+++ controllers/empController.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const Employee = require("../models/Employee");
-const { upload } = require("../configs/cloudinaryConfig");
+const { upload, cloudinary } = require("../configs/cloudinaryConfig");
 
 // add Employee
 exports.addEmployee = async (req, res, next) => {
@@ -16,6 +16,28 @@ exports.addEmployee = async (req, res, next) => {
     res.status(201).json({
       success: true,
       data: newEmployee,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.deleteEmployee = async (req, res, next) => {
+  try {
+    const empId = req.params.id;
+    const employee = await Employee.findByIdAndDelete(empId);
+
+    if (!employee) {
+      return res.status(404).json({
+        success: false,
+        message: "Employee not found",
+      });
+    }
+
+    await cloudinary.uploader.destroy(employee.photo.public_id);
+    res.status(200).json({
+      success: true,
+      message: "Employee deleted successfully",
     });
   } catch (error) {
     next(error);
--- routes/empRoutes.js
+++ routes/empRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { addEmployee } = require("../controllers/empController");
+const { addEmployee, deleteEmployee } = require("../controllers/empController");
 const { upload } = require("../configs/cloudinaryConfig");
 const router = express.Router();
 
 router.post("/add", upload.single("photo"), addEmployee);
+router.delete("/delete/:id", deleteEmployee);
 
 module.exports = router;
